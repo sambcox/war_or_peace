00c341c2ece3e99f8dc28e6a472c603f42eb02d6
Samuel Cox <samc1253@gmail.com>
2026-01-15 21:04:27 -0700

    Create rank of card at feature

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe Deck do
 
     expect(deck.cards).to eq([card1, card2, card3])
   end
+
+  it "can rank cards at specific indices" do
+    card1 = Card.new(:heart, '3', 3)
+    card2 = Card.new(:diamond, 'Queen', 12)
+    card3 = Card.new(:spade, '5', 5)
+
+    deck = Deck.new([card1, card2, card3])
+
+    expect(deck.rank_of_card_at(0)).to eq(3)
+    expect(deck.rank_of_card_at(1)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(5)
+  end
 end
