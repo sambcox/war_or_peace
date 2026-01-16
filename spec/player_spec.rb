41446354f23f454f7e35ee7a6ad201036801ad67
Samuel Cox <samc1253@gmail.com>
2026-01-15 21:31:40 -0700

    Add test for player has lost

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -17,4 +17,20 @@ RSpec.describe Player do
     expect(player.deck).to eq(deck)
     expect(player.deck.cards).to eq([card1, card2, card3])
   end
+
+  it "can determine if a player has lost" do
+    card1 = Card.new(:heart, '3', 3)
+    card2 = Card.new(:diamond, 'Queen', 12)
+    card3 = Card.new(:spade, '5', 5)
+
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new("Clarisa", deck)
+
+    expect(player.has_lost?).to eq(false)
+
+    empty_deck = Deck.new([])
+    player2 = Player.new("Jordan", empty_deck)
+
+    expect(player2.has_lost?).to eq(true)
+  end
 end
