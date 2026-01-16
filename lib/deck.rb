c2bc17876540f85bf1075a5026c1e20111123a0d
Samuel Cox <samc1253@gmail.com>
2026-01-15 21:09:02 -0700

    Finish deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,22 @@ class Deck
   def rank_of_card_at(index)
     @cards[index].rank
   end
+
+  def high_ranking_cards
+    @cards.select { |card| card.rank >= 11 }
+  end
+
+  def percent_high_ranking
+    return 0.0 if @cards.empty?
+
+    (high_ranking_cards.count.to_f / @cards.count.to_f * 100).round(2)
+  end
+
+  def remove_card
+    @cards.shift
+  end
+
+  def add_card(card)
+    @cards << card
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -30,4 +30,50 @@ RSpec.describe Deck do
     expect(deck.rank_of_card_at(1)).to eq(12)
     expect(deck.rank_of_card_at(2)).to eq(5)
   end
+
+  it "can identify high ranking cards" do
+    card1 = Card.new(:heart, '3', 3)
+    card2 = Card.new(:diamond, 'Queen', 12)
+    card3 = Card.new(:spade, '5', 5)
+
+    deck = Deck.new([card1, card2, card3])
+
+    expect(deck.high_ranking_cards).to eq([card2])
+  end
+
+  it "can calculate percentage of high ranking cards" do
+    card1 = Card.new(:heart, '3', 3)
+    card2 = Card.new(:diamond, 'Queen', 12)
+    card3 = Card.new(:spade, '5', 5)
+
+    deck = Deck.new([card1, card2, card3])
+
+    expect(deck.percent_high_ranking).to eq(33.33)
+  end
+
+  it "can remove the top card from the deck" do
+    card1 = Card.new(:heart, '3', 3)
+    card2 = Card.new(:diamond, 'Queen', 12)
+    card3 = Card.new(:spade, '5', 5)
+
+    deck = Deck.new([card1, card2, card3])
+
+    removed_card = deck.remove_card
+
+    expect(removed_card).to eq(card1)
+    expect(deck.cards).to eq([card2, card3])
+  end
+
+  it "can add a card to the bottom of the deck" do
+    card1 = Card.new(:heart, '3', 3)
+    card2 = Card.new(:diamond, 'Queen', 12)
+    card3 = Card.new(:spade, '5', 5)
+    card4 = Card.new(:club, 'Ace', 14)
+
+    deck = Deck.new([card1, card2, card3])
+
+    deck.add_card(card4)
+
+    expect(deck.cards).to eq([card1, card2, card3, card4])
+  end
 end
